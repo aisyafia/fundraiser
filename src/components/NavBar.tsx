f436de781ef353d454eeb773e6a7f21ed40040a9
aisyafia <aisya.lutfia@gmail.com>
2023-07-11 14:23:28 +0200

    fixed responsive navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,46 +4,81 @@ import {
   Container,
   Toolbar,
   Typography,
-  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
 } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [anchorNav, setAnchorNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNav = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorNav(e.currentTarget);
+  };
+
+  const handleCloseNav = () => {
+    setAnchorNav(null);
+  };
+
+  const openGoFundMe = (e: React.MouseEvent<HTMLElement>) => {
+    window.open(
+      "https://www.gofundme.com/f/reusable-menstrual-kit-for-350-girls-in-samburu?utm_campaign=p_lico+share-sheet&utm_location=DASHBOARD&utm_medium=copy_link&utm_source=customer",
+      "_blank"
+    );
+  };
+
   const NavBarItems = [
     { href: "/goal", title: "Our Goal" },
     { href: "/supportus", title: "Support Us" },
     { href: "/team", title: "Our Team" },
   ];
   return (
-    <Box>
-      <AppBar position="static">
-        <Container
-          sx={{ display: "flex", justifyContent: "space-between", flexGrow: 3 }}
-        >
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "6", md: "flex" },
-                fontFamily: "Quicksand",
-                fontWeight: 400,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Quicksand",
+              fontWeight: 400,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Samburu Menstrual Kit Project
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              color="inherit"
+              aria-haspopup="true"
+              onClick={handleOpenNav}
             >
-              Samburu Menstrual Kit Project
-            </Typography>
-            <Box>
-              <Grid>
-                {NavBarItems.map((item) => {
-                  return (
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+              keepMounted
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
+              open={Boolean(anchorNav)}
+              onClose={handleCloseNav}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {NavBarItems.map((item) => {
+                return (
+                  <MenuItem key={item.title} onClick={handleCloseNav}>
                     <NavLink
-                      key={item.title}
                       to={`${item.href}`}
                       style={{
                         textDecoration: "none",
@@ -56,14 +91,90 @@ const NavBar = () => {
                     >
                       {item.title}
                     </NavLink>
-                  );
-                })}
-              </Grid>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+                  </MenuItem>
+                );
+              })}
+              <MenuItem>
+                <Button
+                  onClick={openGoFundMe}
+                  sx={{
+                    display: "block",
+                    margin: "0.75rem",
+                    padding: "0.25rem",
+                    color: "#720026",
+                    fontWeight: 800,
+                    fontSize: "large",
+                    backgroundColor: "Coral",
+                    "&:hover": {
+                      color: "inherit",
+                    },
+                  }}
+                >
+                  Donate now
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "Quicksand",
+              fontWeight: 400,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Samburu Menstrual Kit Project
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {NavBarItems.map((item) => {
+              return (
+                <Button key={item.title} sx={{ display: "block" }}>
+                  <NavLink
+                    to={`${item.href}`}
+                    style={{
+                      textDecoration: "none",
+                      margin: "0.75rem",
+                      padding: "0.25rem",
+                      color: "Coral",
+                      fontWeight: 800,
+                      fontSize: "large",
+                    }}
+                  >
+                    {item.title}
+                  </NavLink>
+                </Button>
+              );
+            })}
+
+            <Button
+              onClick={openGoFundMe}
+              sx={{
+                display: "block",
+                margin: "0.75rem",
+                padding: "0.25rem",
+                color: "#720026",
+                fontWeight: 800,
+                fontSize: "large",
+                backgroundColor: "Coral",
+                "&:hover": {
+                  color: "inherit",
+                },
+              }}
+            >
+              Donate now
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
